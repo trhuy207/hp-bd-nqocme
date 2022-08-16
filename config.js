3b958591a804812e4b1fb1eed11a7e8cff5a76fc
williamtran207 <83764817+trhuy207@users.noreply.github.com>
2022-08-16 08:03:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Ngọc My';
 const giftUrl = 'https://www.facebook.com/nqocmecutes1tg';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng Bé Ngọc My';
-const contentCard = 'Chúc bé My sinh nhật tràn ngập niềm vui. Chúc em gặp nhiều may mắn, học giỏi hơn, đạt học sinh giỏi trong năm học này và đỗ vào trường cấp 3 như ý nha kkk';
+const titleCard = 'Tặng em Ngọc My';
+const contentCard = 'Chúc My sinh nhật tràn ngập niềm vui. Chúc em gặp nhiều may mắn, học giỏi hơn, đạt học sinh giỏi trong năm học này và đỗ vào trường cấp 3 như ý nha kkk';
 const giftImage = 'honey.JPG';
 const base64 = '';
 const giftImageBase64 = "data:image/jpeg;base64, " + base64;
